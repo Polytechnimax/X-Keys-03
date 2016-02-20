40023d4c0ea21b8c5a16fa82446ca0d253525a8a
Polytechnimax <maxime.larcher@polytechnique.edu>
2016-02-20 10:17:32 +0100

    Started working on a new view to open the door (Locks) to be continued.

---
+++ X-Keys 03/LockArray.swift
@@ -0,0 +1,9 @@
+//
+//  LockList.swift
+//  X-Keys 03
+//
+//  Created by LARCHER Maxime on 18/02/2016.
+//  Copyright © 2016 LARCHER Maxime. All rights reserved.
+//
+
+import Foundation
---
+++ X-Keys 03/LockOpenerView.swift
@@ -0,0 +1,9 @@
+//
+//  LockOpenerView.swift
+//  X-Keys 03
+//
+//  Created by LARCHER Maxime on 18/02/2016.
+//  Copyright © 2016 LARCHER Maxime. All rights reserved.
+//
+
+import Foundation
